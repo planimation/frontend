a0dbdaf03a149684edf6d8d9a518dd8af6d92342
xinzhel <l383562940@gmail.com>
2020-10-05 14:54:11 +1100

    [46] Add API documentation; Change some variable and file names

--- planimation.py
+++ planimation.py
@@ -5,8 +5,8 @@ import planimation_api as api
 USAGE_STRING = """
 No command-line options given. Usage:
 
-cli_planimation.py submitVFG 		[obsolute path of vfg file] [download option (png/webm/gif)]
-cli_planimation.py submitPlan 		[obsolute path of domain file] [obsolute path of problem file] [obsolute path of animation file] [download option (png/webm/gif)]
+planimation.py submitVFG 		[absolute path of vfg file] [download option (png/webm/gif/mp4)]
+planimation.py submitPDDL 		[absolute path of domain file] [absolute path of problem file] [absolute path of animation file] [download option (vfg/png/webm/gif/mp4)]
 """
 
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 					i+=1
 					downloadtype = sys.argv[i].strip()
 					
-					if downloadtype!='png' and downloadtype!='webm' and downloadtype!='gif':
+					if downloadtype!='png' and downloadtype!='webm' and downloadtype!='gif' and downloadtype!='mp4':
 						print("invalid download type")
 						exit(1)
 					else:
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 				print("Invalid argument entered for submitVFG")
 				exit(1)
 		# check submitPlan function 		
-		elif sys.argv[i] == "submitPlan":
+		elif sys.argv[i] == "submitPDDL":
 			i+=1
 			if len(sys.argv) == 6:
 				domainFile = sys.argv[i].strip()
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 				if domainFile.endswith('.pddl') and problemFile.endswith('.pddl') and animationFile.endswith('.pddl'):
 					i+=1
 					downloadtype = sys.argv[i].strip()
-					if downloadtype!='png' and downloadtype!='webm' and downloadtype!='gif':
+					if downloadtype!='png' and downloadtype!='webm' and downloadtype!='gif' and downloadtype!='mp4' and downloadtype!='vfg':
 						print("invalid download type")
 						exit(1)
 					else:
@@ -63,7 +63,6 @@ if __name__ == "__main__":
 				else:
 					print("invalid input for pddls,please check your pddl files")
 					exit(1)
-
 
 			else:
 				print("Invalid argument numbers entered for submitPlan")
